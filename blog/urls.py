509aa13021bbc5b14e863c76f410b36ebba8c51a
GeorgePh0 <george.philip0020@gmail.com>
2024-02-13 10:59:25 +0000

    Add html and url to urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
+    path('edit_comment/<str:slug>/<int:comment_id>/', views.edit_comment, name='edit_comment'),
     path('delete_comment/<str:slug>/<int:comment_id>/', views.delete_comment, name='delete_comment'),
 ]
