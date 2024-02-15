a1c666bb0ad403bfdb3080facb6d3eee66a23cfd
GeorgePh0 <george.philip0020@gmail.com>
2024-02-15 15:55:38 +0000

    Add javascript to edit the message post

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,1 +1,15 @@
-console.log('script loaded');
+const commentEditButtons = document.getElementsByClassName("btn-edit");
+const commentTextElement = document.getElementById("id_body");
+const commentEditForm = document.getElementById("comment-form");
+const submitButton = document.getElementById("submitButton");
+
+for (let button of commentEditButtons) {
+    button.addEventListener("click", (e) => {
+        console.log('edit button clicked')
+        let commentId = e.target.getAttribute("comment_id");
+        let commentContent = document.getElementById(`comment${commentId}`).innerText;
+        commentTextElement.value = commentContent;
+        submitButton.innerText = "Update";
+        commentEditForm.setAttribute("action", `edit_comment/${commentId}`);
+    });
+}
